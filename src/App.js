5df7e1e5919a3c0f66130f07bef721587d03913d
air <s5935512030@phuket.psu.ac.th>
2020-03-05 23:07:22 +0700

    complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react';
+import { useTranslation } from 'react-i18next';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reacts
-        </a>
-      </header>
+function MyComponent() {
+  const { t, i18n } = useTranslation();
+  const switchingLanguage = () => {
+    if (i18n.language === "en") {
+      i18n.changeLanguage("th");
+    } else {
+      i18n.changeLanguage("en");
+    }
+  };
+  return <div>
+    <h1>{t('title')}</h1>
+    hh
+    <button type="button" onClick={switchingLanguage}>changeLanguage</button>
     </div>
+}
+
+export default function App() {
+  return (
+    <Suspense fallback="loading">
+      <MyComponent />
+    </Suspense>
   );
 }
 
-export default App;
